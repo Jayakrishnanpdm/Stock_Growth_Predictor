1b1376dc68fa18afe8a12626a0006929fcd7e9af
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-05-08 00:36:37 +0530

    update utils

--- utils/types.py
+++ utils/types.py
@@ -1,5 +1,14 @@
 from enum import Enum
 
-class DatasetPath(Enum):
-    AMAZON = "data/amazon.csv"
-    TESLA = "data/tesla.csv"
+
+class StockSymbols(Enum):
+    META = "Meta"
+    APPLE = "AAPL"
+    MICROSOFT = "MSFT"
+    AMAZON = "AMZN"
+    ALPHABET = "GOOGL"
+    IBM = "IBM"
+    TESLA = "TSLA"
+
+    def get_all_symbols():
+        return {symbol.name: symbol.value for symbol in StockSymbols}
