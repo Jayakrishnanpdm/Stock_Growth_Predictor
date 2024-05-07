775f7249f8cc74b2b4b1561e0ccb06aa71c57dad
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-05-08 00:36:21 +0530

    feat: adding train script

---
+++ train.py
@@ -0,0 +1,19 @@
+import os
+import requests
+from decouple import config
+from utils.types import StockSymbols
+from utils.prediction_utils import StockTrainModel
+
+API_KEY = config("API_KEY")
+
+os.makedirs("data", exist_ok=True)
+
+for key, value in StockSymbols.get_all_symbols().items():
+    URL = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&datatype=csv&outputsize=full&symbol={value}&apikey={API_KEY}"
+    folderpath = f"data/{key}"
+    os.makedirs(folderpath, exist_ok=True)
+    response = requests.get(URL)
+    with open(f"{folderpath}/data.csv", "wb") as file:
+        file.write(response.content)
+    model = StockTrainModel().all_operations(folderpath)
+    print(f"Model trained for {key}")
